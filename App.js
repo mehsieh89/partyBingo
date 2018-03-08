f15f2c40bb3e987667773a353f0931834ab29502
Mel hsieh <mehsieh89@gmail.com>
2018-03-08 13:12:36 -0800

    initialize, board test

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import BingoBoard from './components/bingoBoard.js';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <BingoBoard/>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ components/bingoBoard.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+export default class BingoBoard extends React.Component {
+
+  render() {
+    let board = [
+          [0, 1, 2, 3, 4],
+          [5, 6, 7, 8, 9],
+          [10, 11, 'free', 13, 14],
+          [15, 16, 17, 18, 19],
+          [20, 21, 22, 23, 24]
+        ]
+    return (
+      <View style={styles.board}>
+        {
+          board.map((row, rowIndex) => {
+            return row.map(function(col, colIndex) {
+              return (
+                <View style={styles.text} key={colIndex}>
+                  <Text> {board[rowIndex][colIndex]} </Text>
+                </View>)
+            })
+          })
+        }
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  board: {
+    marginLeft: 15,
+    flex: 1,
+    flexDirection: "row",
+    backgroundColor: '#fff',
+    flexWrap: "wrap",
+  },
+  text: {
+    height: 70,
+    width: 70,
+    borderRadius: 2,
+    borderWidth: 2,
+  }
+});
