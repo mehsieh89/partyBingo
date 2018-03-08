ce8988f969e63ddfc379a388479e629588f13fb1
Mel hsieh <mehsieh89@gmail.com>
2018-03-08 15:46:13 -0800

    adding more options

--- components/options.js
+++ components/options.js
@@ -4,7 +4,8 @@ export const options = ["spills a drink", "complains about their work",
 "calls for a round of shots", "starts singing", "starts dancing", "starts throwing up",
 "says to quiet down", "starts making out with someone else", "starts crying",
 "has a new best friend", "starts stripping", "is having a deep talk", "taking selfies",
-"trips and falls",
+"trips and falls", "makes it rain", "gets a phone number", "is drunk texting",
+"gets rejected",
 ]
 
-//23
+//27
